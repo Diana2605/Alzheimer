b217b58253b69b4812e9f744b3ed95169a5b736a
andcsie <andrei.rizea20@gmail.com>
2020-06-26 12:06:53 +0300

    new route sample for interaction

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//import interaction functionality
+const Interaction = require('./interaction');
 
 app.get('/', (req, res) => res.send('Welcome to my chatbot!'))
+
+//interaction route
+app.post('/interaction', (req, res) => {
+  //here call interaction.callWatsonAssistant function
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
