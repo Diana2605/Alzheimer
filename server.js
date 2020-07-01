ef60e59483cd6ccf2a0232458591d1e14a685f32
Diana2605 <52744854+Diana2605@users.noreply.github.com>
2020-07-01 16:37:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => res.send('Welcome to my chatbot!'))
 app.post('/interaction', (req, res) => {
   //here call interaction.callWatsonAssistant function
   //Interaction.callWatsonAssistant(req.body)
-  Interaction.callWatsonAssistant.message(params, [callback()]);
+  Interaction.callWatsonAssistant(params, [callback()]);
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
